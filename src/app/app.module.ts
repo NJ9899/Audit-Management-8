ced293d860c8bbcae3a996981309ec7b883e49e2
ustjavafsdb209 <namithajoseph99@gmail.com>
2022-11-24 15:49:11 +0530

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ChecklistComponent } from './checklist/checklist.component';
+import { AuditStatusComponent } from './audit-status/audit-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChecklistComponent,
+    AuditStatusComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/audit-status/audit-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-status',
+  templateUrl: './audit-status.component.html',
+  styleUrls: ['./audit-status.component.css']
+})
+export class AuditStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
