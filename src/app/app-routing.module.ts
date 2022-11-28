fc31df429deca7232dcfd7d9aaf03f801c7efeaa
ustjavafsdb205 <233686@ust-global.com>
2022-11-28 15:44:34 +0530

    login committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuditStatusComponent } from './audit-status/audit-status.component';
+import { ChecklistComponent } from './checklist/checklist.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component : LoginComponent },
+  { path: "checklist", component : ChecklistComponent },
+  { path: "audit-status", component : AuditStatusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
