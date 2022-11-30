0c9ad273ff5d140b60f0ee2b5494345c2e40dd92
muhsinmhdkp <233673@ust-global.com>
2022-11-30 17:46:44 +0530

    final

--- src/app/audit-status/audit-status.service.ts
+++ src/app/audit-status/audit-status.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable}  from "rxjs/Observable";
+import { Result } from './result';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,21 @@ export class AuditStatusService {
 
   constructor(private http:HttpClient) { }
 
-  public getAuditStatus():any{
+  // public getAuditStatus():Observable<Result>{
+  //   return this.http.get<Result>("http://localhost:9631/auditstatus");
+  // }
+  public getAuditStatus():Observable<Result[]>{
+    console.log("Inside getAuditStatus")
+    return this.http.get<Result[]>("http://localhost:9631/auditstatus");
+  }
+
+  public getAuditStatus1(){
+    console.log("Inside getAuditStatus")
     return this.http.get("http://localhost:9631/auditstatus");
   }
+
+  
+
 }
 
 
